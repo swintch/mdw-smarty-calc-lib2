3d3cb450712af7d82f3e07c0d8bcf306ce5e670f
Scott Wintch <scott@smarty.com>
2024-11-26 10:31:55 -0700

    adding new functionality to the calculator

--- calc/calc.go
+++ calc/calc.go
@@ -4,9 +4,14 @@ type Calculator interface {
 	Calculate(a, b int) int
 }
 
-type Addition struct {
-}
+type Addition struct{}
+type Subtraction struct{}
+type Multiplication struct{}
+type Division struct{}
 
 func (t *Addition) Calculate(a, b int) int {
 	return a + b
 }
+func (t *Subtraction) Calculate(a, b int) int    { return a - b }
+func (t *Multiplication) Calculate(a, b int) int { return a * b }
+func (t *Division) Calculate(a, b int) int       { return a / b }
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -1,6 +1,9 @@
 package calc
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAddition_Calculate(t1 *testing.T) {
 	type args struct {
@@ -29,10 +32,103 @@ func TestAddition_Calculate(t1 *testing.T) {
 	}
 }
 
-func TestAdditionAnotherWay_Calculate(t1 *testing.T) {
-	calculator := &Addition{}
-	result := calculator.Calculate(1, 2)
-	if result != 3 {
-		t1.Errorf("Calculate() = %v, want %v", result, 3)
+func TestSubtraction_Calculate(t1 *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "5-2=3", args: args{a: 5, b: 2}, want: 3},
+		{name: "1-1=0", args: args{a: 1, b: 1}, want: 0},
+		{name: "0-0=0", args: args{a: 0, b: 0}, want: 0},
+		{name: "0--2=2", args: args{a: 0, b: -2}, want: 2},
+		{name: "-2--2=0", args: args{a: -2, b: -2}, want: 0},
+		{name: "-2-0=-2", args: args{a: -2, b: 0}, want: -2},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := &Subtraction{}
+			if got := t.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t1.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiplication_Calculate(t1 *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "2*2=4", args: args{a: 2, b: 2}, want: 4},
+		{name: "0*1=0", args: args{a: 0, b: 1}, want: 0},
+		{name: "1*5=5", args: args{a: 1, b: 5}, want: 5},
+		{name: "0*0=0", args: args{a: 0, b: 0}, want: 0},
+		{name: "-2*-2=-4", args: args{a: -2, b: -2}, want: 4},
+		{name: "-2*2=-4", args: args{a: -2, b: 2}, want: -4},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := &Multiplication{}
+			if got := t.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t1.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision_Calculate(t1 *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "2/2=1", args: args{a: 2, b: 2}, want: 1},
+		{name: "0/1=0", args: args{a: 0, b: 1}, want: 0},
+		{name: "5/2=2", args: args{a: 5, b: 2}, want: 2},
+		{name: "5/-2=-2", args: args{a: 5, b: -2}, want: -2},
+		{name: "-5/2=-2", args: args{a: -5, b: 2}, want: -2},
+		{name: "-4+-2=2", args: args{a: -4, b: -2}, want: 2},
+	}
+	for _, tt := range tests {
+		t1.Run(tt.name, func(t1 *testing.T) {
+			t := &Division{}
+			if got := t.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t1.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivideByZero_Calculate(t1 *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			errorMessage := fmt.Sprintf("%v", err)
+			panicDivideByZeroMessage := "runtime error: integer divide by zero"
+			assertEquals(t1, errorMessage, panicDivideByZeroMessage)
+		}
+	}()
+	calculator := &Division{}
+	calculator.Calculate(5, 0)
+
+}
+
+func assertEquals(t *testing.T, actual, expected string) {
+	t.Helper()
+	if actual != expected {
+		t.Errorf("actual = %s, expected = %s", actual, expected)
 	}
 }
